a3b5866e4f212f492aae135462940d2733bb35f8
Vasilii Ponteleev <neocaptainnemo@gmail.com>
2022-05-24 11:31:04 +0300

    Animator selector fix.

--- app/src/main/java/com/neocaptainnemo/animations23rdmay/FabActivity.kt
+++ app/src/main/java/com/neocaptainnemo/animations23rdmay/FabActivity.kt
@@ -9,8 +9,6 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import com.google.android.material.appbar.AppBarLayout
 
 class FabActivity : AppCompatActivity() {
 
@@ -33,13 +31,6 @@ class FabActivity : AppCompatActivity() {
 
         val menuItemHeight = resources.getDimension(R.dimen.menu_height)
         val menuItemMargin = resources.getDimension(R.dimen.margin_menu_8)
-
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-
-        toolbar.postDelayed({
-            toolbar.isSelected = true
-        }, 2000L)
-
 
         fun closeMenu() {
 
